e505d2df06b9983e74dff2a620ffe18353af2573
mad-monarch18 <dmullen1218@gmail.com>
2023-10-20 11:23:03 -0500

    added file input

--- src/main/java/org/example/DisplayProducts.java
+++ src/main/java/org/example/DisplayProducts.java
@@ -2,14 +2,10 @@ package org.example;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
-public class Main {
+public class DisplayProducts {
     public static void main(String[] args) {
-        System.out.println("Enter item 'SKU' to add to cart");
-        System.out.println("(S)Search"+ " " +"(C)Cart"+ " " +"(B)Back");
-        System.out.println("");
         try {
             //bringing the file into main class
             FileInputStream fis = new FileInputStream("src/main/resources/Inventory.txt");
@@ -22,7 +18,11 @@ public class Main {
             }
         }
         catch(FileNotFoundException ex){
-            System.out.println("Oops, we ran into a problem.");
+                System.out.println("Oops, we ran into a problem.");
+            }
         }
+
+    public DisplayProducts() {
     }
-    }
+}
+
