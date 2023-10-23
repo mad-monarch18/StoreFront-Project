5f07762b10dc88ed07b0655b7ffb215f3b18a06a
mad-monarch18 <dmullen1218@gmail.com>
2023-10-23 00:04:00 -0500

    case 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,11 @@ package org.example;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Mad Monarch Online Store!");
@@ -17,13 +19,16 @@ public class Main {
 
         Scanner scanner = new Scanner (System.in);
         int userChoice = scanner.nextInt();
+        List<Product> productList = Product.loadProductsFromResource("Inventory.txt"); //
 
-        //Products page
+
+
         switch(userChoice){
         case 1:
-        System.out.println("Enter item 'SKU' to add to cart or type letter to choose option below");
+        System.out.println("Search Item by SKU, then add to cart");
         System.out.println("(S)Search"+ " " +"(C)Cart"+ " " +"(B)Back");
         System.out.println("");
+
         try {
             //bringing the file into main class
             FileInputStream fis = new FileInputStream("src/main/resources/Inventory.txt");
@@ -33,14 +38,45 @@ public class Main {
                 String line = fisScanner.nextLine();
                 System.out.println(line);
             }
-            Scanner userChoice1 = new Scanner(System.in);
-            String option = userChoice1.nextLine().toUpperCase();
 
 
+            String option = scanner.nextLine().toUpperCase();
+            switch(option) {
+                case "S":
+                    System.out.println("Enter Product SKU");
+                    String searchInput = scanner.nextLine();
+                    Product foundProduct = findProductBySKU(productList, searchInput);
+                    if (foundProduct != null) {
+                        System.out.println("Found product: ");
+                        System.out.println("SKU: " + foundProduct.getSku() + "Product Name: " + foundProduct.getProductName() + "Price: $" + foundProduct.getPrice());
+                        System.out.print("Add " + foundProduct.getProductName() + " to your cart? (Y/N): ");
+                        String addItemToCart = scanner.next().toUpperCase();
+                        switch (addItemToCart) {
+                            case "Y":
+                                System.out.println(foundProduct.getProductName() + "has been added");
+                                break;
+                            case "N":
+                                System.out.println(foundProduct.getProductName() + "did not go into your cart.");
+                                break;
+                        }
+                    } else {
+                        System.out.println("No item found");
+                    }
 
-        }
+                    break;
+
+                case "C":
+                    System.out.println("Items in your cart");
+                    break;
+
+                case "B":
+                    return;
+            }}
         catch(FileNotFoundException ex){
             System.out.println("Oops, we ran into a problem.");
         }
+
+
     }
+
     }}
